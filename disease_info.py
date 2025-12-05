2898fa80260ec413a53e7daa1d7963e1151e75bb
aalamomar88-star <aalamomar88@gmail.com>
2025-12-05 21:09:29 +0530

    Update disease_info.py

---
+++ disease_info.py
@@ -0,0 +1,34 @@
+#disease_info
+disease_info={
+    "Common Cold":{
+        "description":"A viral upper respiratory infection causing running nose,sore throat,mild fever.",
+        "Causes":"Many viruses (eg.,rhinoviruses).",
+        "when_to_see_doctor":"if high fever (>38.5C),breathing difficulty,symptoms>10 days,or worsening.",
+        "home_remedy":"Rest,fluids,paracetamol/acetaminophen for fever,saline nasal drops." 
+    },
+    "Flu (Influenza)":{
+        "description":"A viral infection with sudden fever,body aches,cough,fatigue.",
+        "causes":"influenza viruses.",
+        "when_to_see_doctor":"Shortness of breath,persistent high fever,fainting,or severe dehydration.",
+        "home_remedy":"Rest,fluids,antipyretics;seek medical care if high risk."
+    },
+    "COVID-19":{
+        "description":"Respiratory illness caused by SARS-CoV-2;symptoms vary from mild to severe.",
+        "causes":"Coronavirus infection.",
+        "when_to_see_doctor":"Difficulty breathing,persistent chest pain,confusion,blue lips/face.",
+        "home_remedy":"isolation,test when indicated,hydration,antipyretics for fever."
+
+    },
+    "Malaria":{
+        "description":"Parasitic blood infection causing cyclical fevers,chills,sweats,headache.",
+        "causes":"Plasmodium species via mosquito bites.",
+        "when_to_see_doctor":"Immediately if suspected-requires lab tests and treatment.",
+        "home_remedy":"No home remedy-urgent medical treatment required."
+    },
+    "Gastroenteritis":{
+        "description":"Stomach/intestinal inflammation causing diarrhea,vomiting,abdominal pain.",
+        "causes":"Viruses,bacteria,parasites;food poisoning. ",
+        "when_to_see_doctor":"Severe dehydration,bloody stool,high fever.",
+        "home_remedy":"Oral rehydration,rest,BRAT diet; seek care if severe."
+    }
+}
